28a72e1bd3bde61de69de7e494f9792f1ec84622
Kevin Chen <kchen651@atl1-1-01-005-2-1.pace.gatech.edu>
2026-09-25 01:11:07 -0400

    Feat: Complete 6a, 6b, 6c

--- lab1/de_stage.v
+++ lab1/de_stage.v
@@ -207,16 +207,27 @@ end
    reg  [`DBITS-1:0] sxt_imm_DE;
 always @(*) begin 
   case (type_immediate_DE )  
-  `I_immediate: sxt_imm_DE = {{21{inst_DE[31]}}, inst_DE[30:25], inst_DE[24:21], inst_DE[20]};
+  `I_immediate: sxt_imm_DE = {{21{inst_DE[31]}}, inst_DE[30:25], inst_DE[24:21], inst_DE[20]}; // Why not just inst_DE[30:25]?
    `B_immediate: sxt_imm_DE = {{20{inst_DE[31]}}, inst_DE[7], inst_DE[30:25], inst_DE[11:8], 1'b0};
-    /*
+
+   // instr[31:25] = imm[11:5]
+   // instr[11:7] = imm[4:0]
   `S_immediate: 
-     sxt_imm_DE =  ... 
+     sxt_imm_DE =  {{20{inst_DE[31]}}, inst_DE[31:25], inst_DE[11:7]}
    `U_immediate: 
-     sxt_imm_DE = ... 
+     sxt_imm_DE = {inst_DE{31:20}, 20'b0}
+
+    /*
+      instr[31:12] = imm[20|10:1|11|19:12]
+
+      instr[31] = imm[31:21]
+      instr[31] = imm[20]
+      instr[19:12] = imm[19:12]
+      instr[20] = imm[11]
+      instr[30:21] = imm[10:1]
+    */
    `J_immediate: 
-    sxt_imm_DE = ... 
-    */ 
+    sxt_imm_DE = {{12{inst_DE[31]}, inst_DE[19:12], instr_DE[20], instr_DE[30:21],  1'b0}
    default:
     sxt_imm_DE = 32'b0; 
   endcase  
@@ -250,13 +261,41 @@ end
                       (op_I_DE == `BLT_I) ||
                       (op_I_DE == `BGE_I) ||
                       (op_I_DE == `BLTU_I) ||
-                      (op_I_DE == `BGEU_I))? 1 : 0;
+                      (op_I_DE == `BGEU_I) ||
+                      (op_I_DE == `JAL_I) ||
+                      (op_I_DE == `JALR_I)) ? 1 : 0;
 
 
   // Task 6(c)
+  /*
+    add, sub xor, or, and, sll, srl, sra, slt, sltu, addi, xori, ori, andi, slli, srli, srai, slti, sltiu, jal, jalr, lui, auipc
+
+    Unused: lb, lh, lbu, lhu
+  */
   assign wr_reg_DE = ((op_I_DE == `ADD_I) ||
+                      (op_I_DE == `SUB_I) ||
+                      (op_I_DE == `XOR_I) ||
+                      (op_I_DE == `OR_I) ||
+                      (op_I_DE == `AND_I) ||
+                      (op_I_DE == `SLL_I) ||
+                      (op_I_DE == `SRL_I) ||
+                      (op_I_DE == `SRA_I) ||
+                      (op_I_DE == `SLT_I) ||
+                      (op_I_DE == `SLTU_I) ||
                       (op_I_DE == `ADDI_I) ||
-                      (op_I_DE == `ANDI_I)) ? ((rd_DE != 0) ? 1 : 0): 0;
+                      (op_I_DE == `XORI_I) ||
+                      (op_I_DE == `ORI_I) ||
+                      (op_I_DE == `ANDI_I) ||
+                      (op_I_DE == `SLLI_I) ||
+                      (op_I_DE == `SRLI_I) ||
+                      (op_I_DE == `SRAI_I) ||
+                      (op_I_DE == `SLTI_I) ||
+                      (op_I_DE == `SLTIU_I) ||
+                      (op_I_DE == `LW_I) ||
+                      (op_I_DE == `JAL_I) ||
+                      (op_I_DE == `JALR_I) ||
+                      (op_I_DE == `LUI_I) ||
+                      (op_I_DE == `AUIPC_I)) ? ((rd_DE != 0) ? 1 : 0): 0;
 
  /* this signal is passed from WB stage */ 
   wire wr_reg_WB; // is this instruction writing into a register file? 
